779e685a86050eed6400df647fee5191f9b28f3f
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-19 12:00:37 -0400

    seed file

---
+++ seed.py
@@ -0,0 +1,32 @@
+"""Seed file to populate pixly db with sample data."""
+
+from models import Image, db
+import datetime
+
+now = datetime.datetime.now()
+
+db.drop_all()
+db.create_all()
+
+image1 = Image(
+    date_time_created=now.strftime("%m/%d/%Y, %H:%M:%S"),
+    date_time_uploaded=now,
+    gps_latitude="23.45",
+    gps_longitude="35.67",
+    make="CANON",
+    model="CANON EOS DIGITAL REBEL XTi",
+    path="img.jpeg",
+    caption="Image caption")
+
+image2 = Image(
+    date_time_created=now.strftime("%m/%d/%Y, %H:%M:%S"),
+    date_time_uploaded=now,
+    gps_latitude="34.05",
+    gps_longitude="-118.25",
+    make="NIKON",
+    model="D850",
+    path="img2.jpeg",
+    caption="")
+
+db.session.add_all([image1, image2])
+db.session.commit()
