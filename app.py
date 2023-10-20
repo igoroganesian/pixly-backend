e621a509f798eeb8c826c0435a4f762c672c11a3
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-20 13:32:47 -0400

    minor corrections

--- app.py
+++ app.py
@@ -13,25 +13,17 @@ load_dotenv()
 AWS_BUCKET_URL = "https://io-pixly.s3.us-west-1.amazonaws.com"
 
 app = Flask(__name__)
-# CORS(app, resources={r"*": {"origins": "http://localhost:3000"}},
-#      supports_credentials=True)
 CORS(app)
 
 app.config['SECRET_KEY'] = "secret"
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     "DATABASE_URL", 'postgresql:///pixly')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True # displays SQL in terminal
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 
 # debug = DebugToolbarExtension(app)
-
-# @app.route('/test-cors', methods=['GET'])
-# def test_cors():
-#     return "CORS headers should be set!"
 
 @app.get("/images")
 def get_images():
@@ -99,7 +91,7 @@ def get_image_by_id(id):
         app.logger.error(f"Error fetching image with id {id}: {e}")
         return jsonify(error=e)
 
-    image_url = S3.get_preassigned_url(image.path)
+    image_url = S3.get_presigned_url(image.path)
     image_data = image.serialize()
 
     return jsonify(images=[{"image_data": image_data, "url": image_url}])
